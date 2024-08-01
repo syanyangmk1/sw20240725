db7d9681738b41967dd70fb563bb86a19aa3180f
syanyangmk1 <notdead965@gmail.com>
2024-08-01 15:03:16 +0900

    06

--- d6/server/app.js
+++ d6/server/app.js
@@ -8,7 +8,15 @@ app.set('view engine', 'ejs');
 app.set('port', 3000);
 
 app.get("/", (req, res)=>{
-    res.redirect('http://google.com');
+    //res.redirect('http://google.com');
+
+    res.writeHead(200, {"content-type":"text/html; charst=UTF-8"});
+    //res.end("안영 세계");
+
+    var obj = {no:120, name:"HONG"};
+    //res.end(JSON.stringify(obj));
+    //send()은 수식 or 문자열을 화면에 보여준다
+    res.send(obj);
 });
 
 const sercer = http.createServer(app);
